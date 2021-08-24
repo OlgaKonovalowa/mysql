074e2964e6eb7a389fcb7fae98d7974389bbd2c3
OlgaKonovalowa <89103195+OlgaKonovalowa@users.noreply.github.com>
2021-08-24 23:18:31 +0300

    Update example.sql

--- example.sql
+++ example.sql
@@ -1,51 +1,200 @@
--- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
---
--- Host: localhost    Database: example
--- ------------------------------------------------------
--- Server version	8.0.26
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+C:\Users\olgak>mysql -u root -p
+Enter password: ****
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 50
+Server version: 8.0.26 MySQL Community Server - GPL
 
---
--- Table structure for table `users`
---
+Copyright (c) 2000, 2021, Oracle and/or its affiliates.
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
 
---
--- Dumping data for table `users`
---
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sample             |
+| sys                |
+| world              |
++--------------------+
+6 rows in set (0.00 sec)
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+mysql> CREATE DATABASE example;
+Query OK, 1 row affected (0.01 sec)
 
--- Dump completed on 2021-08-23  0:00:17
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| example            |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sample             |
+| sys                |
+| world              |
++--------------------+
+7 rows in set (0.00 sec)
+
+mysql> CREATE TABLE users (id SERIAL, name VARCHAR(100) NOT NULL UNIQUE);
+ERROR 1046 (3D000): No database selected
+mysql> USE example users (id SERIAL, name VARCHAR(100) NOT NULL UNIQUE);
+Database changed
+mysql> SHOW TABLES;
+Empty set (0.00 sec)
+
+mysql> CREATE TABLE users (id SERIAL, name VARCHAR(100) NOT NULL UNIQUE);
+Query OK, 0 rows affected (0.05 sec)
+
+mysql> SHOW TABLES;
++-------------------+
+| Tables_in_example |
++-------------------+
+| users             |
++-------------------+
+1 row in set (0.00 sec)
+
+mysql> exit
+Bye
+
+C:\Users\olgak>mysql -u root -p example
+Enter password: ****
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 51
+Server version: 8.0.26 MySQL Community Server - GPL
+
+Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> user>mysqldump -u root p example >example.sql
+    -> dir
+    ->
+    -> mysql> user>mysqldump -u root -p example > example.sql
+    -> exit
+    -> mysql> exit
+    -> exit
+    -> quit
+    -> \q
+Bye
+
+C:\Users\olgak>mysql -u root -p example
+Enter password: ****
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 52
+Server version: 8.0.26 MySQL Community Server - GPL
+
+Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> mysqldump -u root -p example > example.sql
+    -> \q
+Bye
+
+C:\Users\olgak>mysqldump -u root -p example > example.sql
+Enter password: ****
+
+C:\Users\olgak>dir
+ РўРѕРј РІ СѓСЃС‚СЂРѕР№СЃС‚РІРµ C РёРјРµРµС‚ РјРµС‚РєСѓ Windows
+ РЎРµСЂРёР№РЅС‹Р№ РЅРѕРјРµСЂ С‚РѕРјР°: 2498-0874
+
+ РЎРѕРґРµСЂР¶РёРјРѕРµ РїР°РїРєРё C:\Users\olgak
+
+23.08.2021  00:00    <DIR>          .
+23.08.2021  00:00    <DIR>          ..
+21.08.2021  22:39    <DIR>          .config
+21.08.2021  22:39                59 .gitconfig
+21.08.2021  22:39    <DIR>          .ssh
+21.08.2020  22:14    <DIR>          3D Objects
+04.03.2019  22:28    <DIR>          ansel
+07.06.2021  23:37    <DIR>          ARISExpress
+21.08.2020  22:14    <DIR>          Contacts
+22.08.2021  23:48    <DIR>          Desktop
+21.08.2021  23:07    <DIR>          Documents
+16.08.2021  23:45    <DIR>          Downloads
+23.08.2021  00:00             1В 946 example.sql
+21.08.2020  22:14    <DIR>          Favorites
+22.08.2021  23:28    <DIR>          Google Р”РёСЃРє
+19.08.2019  22:49    <DIR>          HP
+21.08.2020  22:14    <DIR>          Links
+21.08.2020  22:14    <DIR>          Music
+06.08.2021  09:26    <DIR>          OneDrive
+15.07.2021  20:46    <DIR>          Pictures
+21.08.2020  22:14    <DIR>          Saved Games
+21.08.2020  22:14    <DIR>          Searches
+23.07.2021  07:53    <DIR>          Videos
+               2 С„Р°Р№Р»РѕРІ          2В 005 Р±Р°Р№С‚
+              21 РїР°РїРѕРє  17В 759В 461В 376 Р±Р°Р№С‚ СЃРІРѕР±РѕРґРЅРѕ
+
+C:\Users\olgak>mysql -u root -p
+Enter password: ****
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 54
+Server version: 8.0.26 MySQL Community Server - GPL
+
+Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> CREATE DATABASE sample;
+ERROR 1007 (HY000): Can't create database 'sample'; database exists
+mysql> DROP DATABASE sample;
+Query OK, 1 row affected (0.03 sec)
+
+mysql> CREATE DATABASE sample;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> exit
+Bye
+
+C:\Users\olgak>mysql -u root -p sample < example.sql
+Enter password: ****
+
+C:\Users\olgak>mysql -u root -p
+Enter password: ****
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 56
+Server version: 8.0.26 MySQL Community Server - GPL
+
+Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> USE sample;
+Database changed
+mysql> SHOW TABLES;
++------------------+
+| Tables_in_sample |
++------------------+
+| users            |
++------------------+
+1 row in set (0.00 sec)
+
+mysql> exit
+Bye
+
+C:\Users\olgak>
